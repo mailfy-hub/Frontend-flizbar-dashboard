960ea3742776fdd76596339c2e9015bdec1cdd50
Olávio Timóteo <olaviotimoteo@hotmail.com>
2024-07-08 11:38:15 -0300

    feat: Add the user data request by id to the user-related file

--- src/client/users.ts
+++ src/client/users.ts
@@ -8,10 +8,36 @@ export interface IUser {
   email?: "string"
 }
 
+export interface IUserById {
+  avatar: string,
+  createdAt: string,
+  deletedAt: string,
+  email: string,
+  id: string,
+  isAdmin: boolean,
+  lastAccess: string,
+  name: string,
+  password: string,
+  phone: string,
+  surname: string,
+  updatedAt: string,
+  username: string
+  verified: boolean
+}
+
 export const updateUser = async (user: IUser, id: string) => {
   try {
     await api.put(`/users/${id}`, user)
     return
+  } catch (error) {
+    throw new Error(error as string);
+  }
+}
+
+export const getUserById = async (id: string) => {
+  try {
+    const { data } = await api.get(`/users/${id}`)
+    return data
   } catch (error) {
     throw new Error(error as string);
   }
