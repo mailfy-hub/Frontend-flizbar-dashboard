f8e07f6c6474e62f09748f0927c3c24d6335a802
Marlon Lencina DevMarlon <marlonbmoraez@gmail.com>
2024-07-18 17:21:16 -0300

    Fix tables content

--- src/pages/Dashboard/withdraw/index.tsx
+++ src/pages/Dashboard/withdraw/index.tsx
@@ -1,6 +1,4 @@
 import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
   TrashIcon,
 } from "@heroicons/react/16/solid";
 import {
@@ -225,17 +223,17 @@ export const Withdraw = () => {
         </CardBody>
         <CardFooter className="flex justify-between items-center">
           <Typography variant="h6" color="blue-gray">
-            Página 2 <span className="font-normal text-BLACK">of 10</span>
+            Página 1 <span className="font-normal text-BLACK">de 1</span>
           </Typography>
           <div className="flex gap-4">
-            <Button variant="text" className="flex items-center gap-1">
+            {/* <Button variant="text" className="flex items-center gap-1">
               <ChevronLeftIcon strokeWidth={3} className="h-3 w-3" />
               Anterior
             </Button>
             <Button variant="text" className="flex items-center gap-1">
               Próximo
               <ChevronRightIcon strokeWidth={3} className="h-3 w-3" />
-            </Button>
+            </Button> */}
           </div>
         </CardFooter>
       </Card>
