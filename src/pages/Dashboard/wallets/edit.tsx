24decc8d25834d2a13bcb01ccbd2273c55010bd7
Gabriela Jauhar <gbjauhar@gmail.com>
2024-08-02 10:51:34 -0300

    FEAT: integration wallets

--- src/pages/Dashboard/wallets/edit.tsx
+++ src/pages/Dashboard/wallets/edit.tsx
@@ -1,11 +1,49 @@
 import { ArrowLeftIcon } from "@heroicons/react/16/solid";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Button, Input, Option, Select } from "@material-tailwind/react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { SectionTitle } from "../../../components/sectionTitle";
+import { api } from "../../../client/api";
+import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from "yup"
 
 export const WalletEdit = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { id, walletName, type } = location.state;
+
+  const validationSchema = Yup.object().shape({
+    walletName: Yup.string().required("Nome é obrigatório"),
+    type: Yup.string().required("Tipo é obrigatório"),
+  });
+
+  type FormValues = Yup.InferType<typeof validationSchema>;
+
+  const initialValues: FormValues = {
+    walletName: walletName,
+    type: type
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
+
+  const handleSubmit = async (data: FormValues) => {
+    try {
+      await api.put(`wallets/${id}`, data)
+      toast.success("Alterada com sucesso!")
+      navigate(-1)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      formik.setSubmitting(false);
+    }
+  }
 
   const handleNavigateBack = () => {
     navigate(-1);
@@ -21,24 +59,42 @@ export const WalletEdit = () => {
         </button>
         <SectionTitle text="Dados da carteira" />
       </div>
-      <form className="mt-12">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="mt-12"
+      >
         <div className="bg-WHITE p-8 w-full rounded-md">
           <div className="flex items-center gap-4">
             <Icon height={16} icon={"heroicons:wallet"} color="black" />
             <SectionTitle size="sm" text="Carteira" />
           </div>
           <div className="mt-8 flex flex-col gap-6 ">
-          <div className="grid md:grid-cols-2 gap-6">
-              <Input type="text" label="Nome da carteira" />
-              <Select label="Tipo do fundo">
-                <Option value="">Convecional</Option>
-                <Option value="">Emergencial</Option>
+            <div className="grid md:grid-cols-2 gap-6">
+              <Input 
+                id="walletName"
+                name="walletName"
+                type="text"
+                label="Nome da carteira"
+                value={formik.values.walletName}
+                onChange={formik.handleChange}
+                error={formik.touched.walletName && Boolean(formik.errors.walletName)}
+              />
+              <Select
+                id="type"
+                name="type"
+                label="Tipo do fundo"
+                value={formik.values.type}
+                onChange={(selectedValue) => formik.setFieldValue("type", selectedValue)}
+                error={formik.touched.type && Boolean(formik.errors.type)}
+              >
+                <Option value="conventional">Convencional</Option>
+                <Option value="emergency">Emergencial</Option>
               </Select>
             </div>
           </div>
         </div>
         <div className="w-full flex justify-end mt-8">
-          <Button className="bg-GOLD_MAIN w-full md:w-auto">
+          <Button type="submit" className="bg-GOLD_MAIN w-full md:w-auto">
             Atualizar dados
           </Button>
         </div>
--- src/pages/Dashboard/wallets/insert.tsx
+++ src/pages/Dashboard/wallets/insert.tsx
@@ -1,8 +1,12 @@
 import { ArrowLeftIcon } from "@heroicons/react/16/solid";
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { Button, Input, Option, Select } from "@material-tailwind/react";
+import { Button, Input, Option, Select, Typography } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 import { SectionTitle } from "../../../components/sectionTitle";
+import { useFormik } from "formik";
+import * as Yup from "yup"
+import { api } from "../../../client/api";
+import { toast } from "react-toastify";
 
 export const WalletInsert = () => {
   const navigate = useNavigate();
@@ -10,6 +14,41 @@ export const WalletInsert = () => {
   const handleNavigateBack = () => {
     navigate(-1);
   };
+
+  const validationSchema = Yup.object().shape({
+    walletName: Yup.string().required("Nome é obrigatório"),
+    type: Yup.string().required("Tipo é obrigatório"),
+  });
+
+  type FormValues = Yup.InferType<typeof validationSchema>;
+
+  const initialValues: FormValues = {
+    walletName: "",
+    type: ""
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
+
+  console.log(formik)
+
+  const handleSubmit = async (data: FormValues) => {
+    try {
+      await api.post("wallets", data)
+      toast.success("Criada com sucesso!")
+      navigate(-1)
+    } catch (error) {
+      console.log(error);
+    } finally {
+      formik.setSubmitting(false);
+    }
+  }
+
   return (
     <div>
       <div className="flex items-center gap-4">
@@ -21,7 +60,10 @@ export const WalletInsert = () => {
         </button>
         <SectionTitle text="Preencha o formulário de inclusão" />
       </div>
-      <form className="mt-12">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="mt-12"
+      >
         <div className="bg-WHITE p-8 w-full rounded-md">
           <div className="flex items-center gap-4">
             <Icon height={16} icon={"heroicons:wallet"} color="black" />
@@ -29,18 +71,42 @@ export const WalletInsert = () => {
           </div>
           <div className="mt-8 flex flex-col gap-6 ">
             <div className="grid md:grid-cols-2 gap-6">
-              <Input type="text" label="Nome da carteira" />
-              <Select label="Tipo do fundo">
-                <Option value="">Convecional</Option>
-                <Option value="">Emergencial</Option>
+              <Input 
+                id="walletName"
+                name="walletName"
+                type="text"
+                label="Nome da carteira"
+                value={formik.values.walletName}
+                onChange={formik.handleChange}
+                error={formik.touched.walletName && Boolean(formik.errors.walletName)}
+              />
+              <Select
+                id="type"
+                name="type"
+                label="Tipo do fundo"
+                value={formik.values.type}
+                onChange={(selectedValue) => formik.setFieldValue("type", selectedValue)}
+                error={formik.touched.type && Boolean(formik.errors.type)}
+              >
+                <Option value="conventional">Convencional</Option>
+                <Option value="emergency">Emergencial</Option>
               </Select>
             </div>
 
           </div>
         </div>
-        <div className="w-full flex justify-end mt-8">
-          <Button className="bg-GOLD_MAIN w-full md:w-auto">
-            Adicionar Carteira
+        <div className={`w-full flex ${formik.isValid ? 'justify-end' : 'justify-between'} mt-8`}>
+          {!formik.isValid && (
+            <Typography variant="small" className="text-red-500">
+              Todos os campos são obrigatórios
+            </Typography>
+          )}
+          <Button
+            className="bg-GOLD_MAIN w-full md:w-auto"
+            type="submit"
+            disabled={formik.isSubmitting}
+          >
+            ADICIONAR CARTEIRA
           </Button>
         </div>
       </form>
