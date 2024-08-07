cabb385b58d6a9fee5f4da32e8b6224b265a3c0d
Olávio Timóteo <olaviotimoteo@hotmail.com>
2024-08-06 23:21:13 -0300

    feat: Add translation of the entire adjustments stream

--- src/utils/translate.tsx
+++ src/utils/translate.tsx
@@ -1,15 +1,11 @@
-// import React from "react";
 import { useTranslation } from "react-i18next";
 
 function Translate(route: string) {
   4;
-  console.log("route", route);
 
   const { t } = useTranslation();
 
   return t("default.routes." + route);
 }
-
-console.log("Translate", Translate("dashboard"));
 
 export default Translate;
